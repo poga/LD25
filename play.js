381dd6780248f024a40fdc565352adf1202cd86f
Poga Po <poga.bahamut@gmail.com>
2012-12-16 23:57:04 +0800

    some game plays

--- play.js
+++ play.js
@@ -7,10 +7,32 @@ var playState = {
     bgOffsetY: 0,
     MAX_WIDTH: 500,
     MAX_HEIGHT: 500,
-    VERTICAL_SPEED: 3,
+    VERTICAL_SPEED: 5,
     context_saved: false,
-    tick: 0,
+    tick: -300,
     translate: false,
+    // Patterns!
+    patterns: [[[1, 1, 1, 1, 1],
+                [0, 1, 1, 1, 0],
+                [0, 0, 1, 0, 0],
+                [0, 0, 1, 0, 0],
+                [1, 1, 1, 1, 1],
+                [0, 1, 1, 1, 0]],
+               [[1, 0, 0, 0, 0],
+                [0, 1, 0, 0, 0],
+                [0, 0, 1, 0, 0],
+                [0, 0, 0, 1, 0],
+                [0, 0, 0, 0, 1]]],
+    patternIndex: 0,
+    patternSubIndex: 0,
+    goodQuotes: [ "I love you.", "Forever.", "Our first kiss.", "First date."],
+    badQuotes: [ "Forgot something important.", "Lying.", "Ignoring" ],
+    // Player Status
+    hp: 50,
+    time: 75,
+    rage: 0,
+
+    dt: 0,
 
     setup: function () {
         "use strict";
@@ -46,10 +68,11 @@ var playState = {
     update: function () {
         "use strict";
         // Keyboard Handling
-        if (jaws.pressed("left")) { this.player.x -= 4; }
-        if (jaws.pressed("right")) { this.player.x += 4; }
+        if (jaws.pressed("left")) { this.player.x -= 12; }
+        if (jaws.pressed("right")) { this.player.x += 12; }
         if (jaws.pressed("z")) {
             this.translate = true;
+            this.rage -= 1;
             this.player.boosting = true;
         } else {
             this.translate = false;
@@ -59,13 +82,15 @@ var playState = {
         this._updatePlayer();
         this._updateBackground();
         this._updateBlocks();
+        this._updateTimer();
 
-        if (this.tick > 50) {
+        if (this.tick > 30) {
             this._addBlockRow();
             this.tick = 0;
         } else {
             this.tick += 1;
         }
+
     },
 
     draw: function () {
@@ -83,11 +108,11 @@ var playState = {
         this.backgrounds.draw();
         this.blocks.draw();
 
-        // Restore the context
         jaws.context.restore();
 
         // These sprites won't be translated
         this.player.draw();
+        this._drawUI();
 
         if (this.translate) {
             jaws.context.save();
@@ -97,12 +122,37 @@ var playState = {
         }
     },
 
+    _drawUI: function () {
+        "use strict";
+
+        var context = jaws.context;
+
+        context.fillStyle = "white";
+        context.font = "bold 20px Arial";
+        context.fillText("HP: " + this.hp, 10, 20);
+        context.fillText("RAGE: " + this.rage, 10, 45);
+        context.fillText("TIME: " + this.time, 10, 70);
+    },
+
+    _updateTimer: function () {
+        "use strict";
+
+        var dt = jaws.game_loop.tick_duration;
+
+        this.dt += dt;
+
+        if (this.dt >= 1000) {
+            this.time -= 1;
+            this.dt = 0;
+        }
+    },
+
     _updatePlayer: function () {
         "use strict";
         this.player.setImage(this.player.anim_default.next());
 
         if (this.player.boosting) {
-            this.player.move(0,-2);
+            this.player.move(0, -2);
         } else {
             if (this.player.y < jaws.height - 50) {
                 this.player.move(0, 2);
@@ -146,18 +196,50 @@ var playState = {
     _addBlockRow: function () {
         "use strict";
 
-        var left = 0, block;
+        var left = 0, block, shouldAddSeparator, i, stillPatternToShow = true;
+
+        // If there is still patterns to show
+        if (this.patterns.length > this.patternIndex) {
+            // If current pattern is completed, go to next pattern
+            console.log(" patterns[i].length: " + this.patterns[this.patternIndex].length);
+            if (this.patternSubIndex >= this.patterns[this.patternIndex].length) {
+                this.patternIndex += 1;
+                this.patternSubIndex = 0;
+                shouldAddSeparator = true;
+                console.log(" should add separator ");
+            }
+        } else {
+            stillPatternToShow = false;
+        }
 
         while (left < this.MAX_WIDTH) {
-            if (Math.random() * 2 > 1) {
+            if (shouldAddSeparator) {
                 block = new jaws.Sprite({image: "good_block.png", x: left, y: -100, anchor: "top_left"});
+                block.collided = false;
+                block.type = "good";
+                this.blocks.push(block);
+                left += 100;
             } else {
-                block = new jaws.Sprite({image: "bad_block.png", x: left, y: -100, anchor: "top_left"});
+                if (stillPatternToShow) {
+                    for (i = 0; i < this.patterns[this.patternIndex][this.patternSubIndex].length; i += 1) {
+                        if (this.patterns[this.patternIndex][this.patternSubIndex][i] === 1) {
+                            block = new jaws.Sprite({image: "good_block.png", x: left, y: -100, anchor: "top_left"});
+                            block.type = "good";
+                        } else {
+                            block = new jaws.Sprite({image: "bad_block.png", x: left, y: -100, anchor: "top_left"});
+                            block.type = "bad";
+                        }
+                        block.collided = false;
+                        this.blocks.push(block);
+                        left += 100;
+                    }
+                    this.patternSubIndex += 1;
+                } else {
+                    break;
+                }
             }
-            block.collided = false;
-            this.blocks.push(block);
-            left += 100;
         }
+
     },
 
     _updateBlocks: function () {
@@ -165,24 +247,33 @@ var playState = {
 
         var self = this;
         this.blocks.forEach(function (block, i) {
-            block.move(0, self.VERTICAL_SPEED);
+            block.move(0, self.VERTICAL_SPEED + 2);
             if (!block.collided) {
                 if (block.y + 100 >= self.player.y) {
                     if (block.x <= self.player.x && block.x + 100 >= self.player.x) {
                         console.log("collide!");
                         block.collided = true;
+                        block.shouldRemove = true;
+
+                        if (!self.player.boosting) {
+                            if (block.type === "good") {
+                                self.hp += 5;
+                            } else {
+                                self.hp -= 5;
+                                self.rage += 10;
+                            }
+                        }
                     }
                 }
             }
         });
 
         this.blocks.removeIf(function (block) {
-            if (block.y > self.MAX_HEIGHT) {
+            if (block.y > self.MAX_HEIGHT || block.shouldRemove) {
                 return true;
             }
             return false;
         });
     }
-}
-
+};
 
